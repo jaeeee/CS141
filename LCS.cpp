9fa35f5ae4d2af4b6be08f219860cb453dfbbb58
Chris Hung Huynh <hhuyn019@ucr.edu>
2020-03-12 00:00:48 -0700

    LCS

--- LCS.cpp
+++ LCS.cpp
@@ -1,38 +1,60 @@
-
-/* A Naive recursive implementation of LCS problem */
-// #include <bits/stdc++.h>
+//#include <bits/std.h>
 #include <iostream>
 using namespace std;
 
 int max(int a, int b);
 
-/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-int lcs(char *X, char *Y, int m, int n)
+int lcs(char *input1, char *input2, int x, int y)
 {
-    if (m == 0 || n == 0)
+    if (x == 0 || y == 0)
         return 0;
-    if (X[m - 1] == Y[n - 1])
-        return 1 + lcs(X, Y, m - 1, n - 1);
+    if (input1[x - 1] == input2[y - 1])
+        return 1 + lcs(input1, input2, x - 1, y - 1);
     else
-        return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
+        return max(lcs(input1, input2, x, y - 1), lcs(input1, input2, x - 1, y));
 }
 
-/* Utility function to get max of 2 integers */
 int max(int a, int b)
 {
     return (a > b) ? a : b;
 }
 
+int lcs_bottom(char *input1, char *input2, int x, int y)  
+{  
+    int out[x + 1][y + 1];  
+    int i, j;  
+    for (i = 0; i <= x; i++)  
+    {  
+        for (j = 0; j <= y; j++)  
+        {  
+        if (i == 0 || j == 0)  
+            out[i][j] = 0;  
+      
+        else if (input1[i - 1] == input2[j - 1])  
+            out[i][j] = out[i - 1][j - 1] + 1;  
+      
+        else
+            out[i][j] = max(out[i - 1][j], out[i][j - 1]);  
+        }  
+    }  
+    return out[x][y];  
+}
+
 /* Driver code */
 int main()
 {
-    char X[] = "AGGTAB";
-    char Y[] = "GXTXAYB";
+    char X[100];
+    char Y[100];
 
+    cout << "Enter X sequence: ";
+    cin >> X;
+    cout << "Enter Y sequence: ";
+    cin >> Y;
+ 
     int m = strlen(X);
     int n = strlen(Y);
 
-    cout << "Length of LCS is " << lcs(X, Y, m, n);
-
+   // cout << "Length of LCS is " << lcs(X, Y, m, n);
+    cout << "Length of LCS is " << lcs_bottom(X,Y,m,n) << endl;
     return 0;
 }
